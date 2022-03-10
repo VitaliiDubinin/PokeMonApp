f0fdfb5bc2273ff6bb1c9dde47daf33c0969ba4a
VitaliiDubinin <vitalii.dubinin@edu.bc.fi>
2022-03-10 19:08:27 +0200

    final PoKe

--- pokeMarg.js
+++ pokeMarg.js
@@ -1,4 +1,4 @@
-const pokedex = document.querySelector(".card");
+const pokedex = document.querySelector(".grid");
 
 // fetch("https://pokeapi.co/api/v2/pokemon?offset=300&limit=100")
 
@@ -6,17 +6,24 @@ const fetchData = () => {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=300")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      poceCards(data.results);
+      //   console.log(data);
+      //   poceCards(data.results);
+      const fetches = data.results.map((p) => {
+        return fetch(p.url).then((res) => res.json());
+      });
+      //   Promise.all(fetches).then((res) => console.log("promisesreq res", res));
+      Promise.all(fetches).then((res) => {
+        poceCards(res);
+      });
     });
 };
 const poceCards = (data) => {
-  console.log("pokecards data coming", data);
+  //   console.log("pokecards data coming", data);
 
   const cards = data
     .map((card) => {
-      return `<div>
-<img src="" alt"" />
+      return `<div class="card">
+<img src="${card.sprites.other.dream_world.front_default}" alt"" />
 <h2>${card.name}</h2>
 </div>`;
     })
